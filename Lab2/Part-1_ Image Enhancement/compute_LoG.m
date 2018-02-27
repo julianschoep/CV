762ae30528a54da24218fef9469d31476b711c62
Julian Schoep <julian.schoep@gmail.com>
2018-02-27 15:29:15 +0100

    back up branch

--- Lab2/Part-1_ Image Enhancement/compute_LoG.m
+++ Lab2/Part-1_ Image Enhancement/compute_LoG.m
@@ -9,7 +9,7 @@ switch LOG_type
     case 2
         %method 2: Convolving the image directly with a LoG kernel (kernel size of 5
         %    and standard deviation of 0.5).
-
+	% bla bla
     case 3
         %method 3: Taking the Difference of two Gaussians (DoG) computed at different
         %    scales σ 1 and σ 2 .
