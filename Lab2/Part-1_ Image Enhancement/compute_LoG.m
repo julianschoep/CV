e8d52f66b2e0b08c3bb0991c5bffbdc17bc0821f
Julian Schoep <julian.schoep@gmail.com>
2018-02-27 15:35:54 +0100

    Juls stuff back in master again

--- Lab2/Part-1_ Image Enhancement/compute_LoG.m
+++ Lab2/Part-1_ Image Enhancement/compute_LoG.m
@@ -2,17 +2,18 @@ function imOut = compute_LoG(image, LOG_type)
 
 switch LOG_type
     case 1
-        %method 1
-        fprintf('Not implemented\n')
-
+        %method 1: Smoothing the image with a Gaussian kernel (kernel size of 5 and
+        %    standard deviation of 0.5), then taking the Laplacian of the smoothed image
+        %    (i.e. second derivative).
+    
     case 2
-        %method 2
-        fprintf('Not implemented\n')
+        %method 2: Convolving the image directly with a LoG kernel (kernel size of 5
+        %    and standard deviation of 0.5).
 
     case 3
-        %method 3
-        fprintf('Not implemented\n')
-
+        %method 3: Taking the Difference of two Gaussians (DoG) computed at different
+        %    scales σ 1 and σ 2 .
+    
 end
 end
 
