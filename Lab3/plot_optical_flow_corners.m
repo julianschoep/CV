5dda7c8882cc4c6c0049f74a393b09cf17087ab8
TijmenvanDijk <31856921+TijmenvanDijk@users.noreply.github.com>
2018-03-09 19:13:01 +0100

    Update section 3

---
+++ Lab3/plot_optical_flow_corners.m
@@ -0,0 +1,42 @@
+% Optical flow through Lucas-Kanade for detected corners (Section 3).
+function plot_optical_flow_corners(original_image, moved_image, region_size, r, c)
+    % Compute image wide spatial and temporal derivatives.
+    % Image wide to reduce the impact of derivative padding.
+    
+    % The center pixel offset of the region borders.
+    pixel = round(region_size/2);
+    
+    % Discard corners that are too close to the edge for the region size.
+    j = 1;
+    for i = 1:numel(r)
+        r_i = r(i);
+        c_i = c(i);
+        if r_i + pixel <= size(original_image, 1) - 1 ... 
+                && c_i + pixel <= size(original_image, 2) - 1 ... 
+                && r_i - pixel >= 1 && c_i - pixel >= 1
+            r(j) = r_i;
+            c(j) = c_i;
+            j = j + 1;
+        end
+    end
+    
+    [Ix, Iy] = gradient(original_image);
+    It = moved_image - original_image;
+    
+    v1 = zeros(size(r));
+    v2 = zeros(size(r));
+    % Compute optical flow vectors per region.
+    for i = 1:numel(r)
+        Ix_i = Ix(r(i)-pixel:r(i)+pixel, c(i)-pixel:c(i)+pixel);
+        Iy_i = Iy(r(i)-pixel:r(i)+pixel, c(i)-pixel:c(i)+pixel);
+        It_i = It(r(i)-pixel:r(i)+pixel, c(i)-pixel:c(i)+pixel);
+        A = [Ix_i(:), Iy_i(:)];
+        b = It_i(:);
+        v = pinv(A)*b;
+
+        v1(i) = v(1);
+        v2(i) = v(2);
+    end
+    % Plot flow vectors on the image.
+    quiver(c, r, v1, v2, 'r');
+end
--- Lab3/tracking.m
+++ Lab3/tracking.m
@@ -1,3 +1,5 @@
+demo;
+
 function tracking( dir_name, s, k, sigma, N, t)
 % Parameters
 % dir_name  : directory where the input images live
@@ -11,18 +13,29 @@ image_filenames = only_images(dir(dir_name));
 n_files = length(image_filenames);
 
 % Read in the first image
-images = imread([dir_name,'/',image_filenames(1,:)]);
+images = im2double(imread([dir_name,'/',image_filenames(1,:)]));
 for i= 2:n_files
     % Read in subsequent images
     url = [dir_name,'/',image_filenames(i,:)];
-    temp_i = imread(url);
+    temp_i = im2double(imread(url));
     images = cat(4,images,temp_i);
 end
 
-
-[H, r,c] = harris_corner_detector(images(:,:,:,1), k,sigma,N,t);
+[H, r, c] = harris_corner_detector(images(:,:,:,1), k,sigma,N,t);
 for i=2:n_files
-    image = images(:,:,:,i);
+    image1 = images(:,:,:,i-1);
+    image2 = images(:,:,:,i);
+    
+    fig = figure(4)
+    imshow(image1)
+    hold on
+    
+    gray_image1 = rgb2gray(image1);
+    gray_image2 = rgb2gray(image2);
+    plot_optical_flow_corners(gray_image1, gray_image2, s, r, c);
+    str = sprintf('animation/%s_%d.jpg', dir_name, i);
+    imwrite(fig, str)
+    
     % For every image
     % determine next point of feature
     % overlay said point on image
@@ -33,11 +46,8 @@ end
 
 function demo
     % still have to determine s
+    s = 5
     tracking( './pingpong', s, 5, 5, 5, 0.1)
     
     tracking( './person_toy', s, 7, 3, 9, 0.02)
 end
-
-
-
-
