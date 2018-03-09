0ade8035dbbe093623b0320755f595690cdd10ab
TijmenvanDijk <31856921+TijmenvanDijk@users.noreply.github.com>
2018-03-09 19:33:20 +0100

    Fixed lucas_kanade

--- Lab3/lucas_kanade.m
+++ Lab3/lucas_kanade.m
@@ -31,7 +31,6 @@ end
 function plot_optical_flow_image(original_image, moved_image, region_size)
     % Compute image wide spatial and temporal derivatives.
     % Image wide to reduce the impact of derivative padding.
-
     [Ix, Iy] = gradient(original_image);
     It = moved_image - original_image;
 
@@ -66,49 +65,6 @@ function plot_optical_flow_image(original_image, moved_image, region_size)
     end
     % Plot flow vectors on the image.
     quiver(x, y, v1, v2, 'r');
-end
-
-% Optical flow through Lucas-Kanade for detected corners (Section 3).
-function plot_optical_flow_corners(original_image, moved_image, region_size, r, c)
-    % Compute image wide spatial and temporal derivatives.
-    % Image wide to reduce the impact of derivative padding.
-    
-    % The center pixel offset of the region borders.
-    pixel = round(region_size/2)
-    
-    % Discard corners that are too close to the edge for the region size.
-    j = 1
-    for i = 1:numel(r)
-        r_i = r(i);
-        c_i = c(i);
-        if r_i + p <= size(original_image, 1) - 1 ... 
-                && c_i + pixel <= size(original_image, 2) - 1 ... 
-                && r_i - pixel >= 1 && c_i - pixel >= 1
-            r(j) = r_i;
-            c(j) = c_i;
-            j = j + 2
-        end
-    end
-    
-    [Ix, Iy] = gradient(original_image);
-    It = moved_image - original_image;
-    
-    v1 = zeros(size(r));
-    v2 = zeros(size(r));
-    % Compute optical flow vectors per region.
-    for i = 1:numel(r)
-        Ix = Ix_m(r-pixel:c+pixel, r-pixel:c+pixel);
-        Iy = Iy_m(r-pixel:c+pixel, r-pixel:c+pixel);
-        It = It_m(r-pixel:c+pixel, r-pixel:c+pixel);
-        A = [Ix(:), Iy(:)];
-        b = It(:);
-        v = pinv(A)*b;
-
-        v1(i) = v(1);
-        v2(i) = v(2);
-    end
-    % Plot flow vectors on the image.
-    quiver(c, r, v1, v2, 'r');
 end
 
 % Divide an image in regions with correct remainders.
